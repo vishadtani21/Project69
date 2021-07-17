9f80fd8de553ec0af8cdd93e50d88d4df3061327
Vishakha Adtani <74194073+vishadtani21@users.noreply.github.com>
2021-07-17 09:23:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+import ScanScreen from './screens/ScanScreen';
+ 
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <ScanScreen />
+      
+    );
+  }
+}
+
+
